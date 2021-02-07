38f7181905fe667486835726728708e470bb65d8
phage <kyp0717@gmail.com>
2021-02-07 06:49:40 -0500

    emacs load packages

---
+++ emacs/init.el
@@ -0,0 +1,36 @@
+
+;; Settings
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+(menu-bar-mode -1)
+; (toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+
+
+; ;; Download Evil
+; (unless (package-installed-p 'evil)
+;   (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs/install.el
@@ -0,0 +1,15 @@
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+
