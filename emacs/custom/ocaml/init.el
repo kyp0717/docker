66ea0da4f76b08e3a83a43757e9506c5d75660cc
Phage Ky <kyp0717@gmail.com>
2021-02-27 09:24:22 -0500

    update config files

--- emacs/custom/ocaml/init.el
+++ emacs/custom/ocaml/init.el
@@ -1,21 +1,7 @@
-
-;; Settings
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-message t)
-(menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
-
 (require 'package)
-; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-; (add-to-list 'package-archives '("gnu"       . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("gnu"       . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-(package-refresh-contents)
-
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)  ; Unless it is already installed
@@ -23,56 +9,32 @@
   (package-install 'use-package))           ; Install latest use-package
 (require 'use-package)
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
-;; Enable Evil
-;; Evil
-(use-package evil :ensure t
-  :init (evil-mode 1)
-  :config
-  (setq evil-emacs-state-cursor '("red" box)
-        evil-normal-state-cursor '("green" box)
-        evil-visual-state-cursor '("orange" box)
-        evil-insert-state-cursor '("blue" bar)
-        evil-replace-state-cursor '("red" bar)
-        evil-operator-state-cursor '("red" hollow)
-
-        evil-cross-lines t)
-  )
-
+(org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
+;; Enable company in all buffers
+;; (add-hook 'after-init-hook 'global-company-mode)
 
 ;; All the icons
-(use-package all-the-icons :ensure t
+(use-package all-the-icons 
   :config (setq inhibit-compacting-font-caches t))
 ; (use-package all-the-icons)
 
-; (use-package doom-modeline
-;     :init (doom-modeline-mode 1)
-;     :custom ((doom-modeline-height 15)))
-
-; (require 'powerline)
-; (powerline-vim-theme)
-; (use-package doom-themes
-;     :init (load-theme 'doom-dracula t))
-
-;; Theme settings
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(use-package gruvbox-theme :ensure t
-  :init
-  ;; Disable old theme before loading
-  (defadvice load-theme
-      (before theme-dont-propagate activate)
-    (mapc #'disable-theme custom-enabled-themes))
-  :config (load-theme 'gruvbox-dark-medium t)
-  )
-
-; (add-to-list 'default-frame-alist '(font .   "Source Code Pro 11" ))
-; (set-fontset-font "fontset-default" nil
-;                   (font-spec :size 15 :name "Dejavu Sans Mono"))
-
-(setq sml/name-width 20)
-(setq sml/theme 'dark)
-(require 'smart-mode-line)
-(sml/setup)
+;;; DO NOT CHANGE BELOW --- emacs generated code -- DO NOT CHANGE
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruvbox-dark-medium))
+ '(custom-safe-themes
+   '("7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "8d5f22f7dfd3b2e4fc2f2da46ee71065a9474d0ac726b98f647bc3c7e39f2819" default))
+ '(package-selected-packages
+   '(evil ido org powerline smart-mode-line swiper ivy use-package tuareg smart-mode-line-powerline-theme projectile org-superstar org-bullets helm gruvbox-theme-dark fzf evil-org evil-commentary evil-collection counsel company command-log-mode beacon all-the-icons airline-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
