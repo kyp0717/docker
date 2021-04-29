a19b6936729fef9910a533e865d6bd933c11eab2
Phat Ky <kyp0717@gmail.com>
2021-04-29 07:11:41 -0400

    add racket mode

---
+++ emacs/racket/install_base2.el
@@ -0,0 +1,20 @@
+;; source for these config ...
+;; https://github.com/sho-87/dotfiles/blob/master/emacs/init.el
+
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+
+
+;; Download Evil
+; (unless (package-installed-p 'evil)
+(package-install 'lispy )
+(package-install 'lispyville)
+(package-install 'flycheck)
+(package-install 'rainbow-delimiters)
+;;(package-install 'uniquify)
